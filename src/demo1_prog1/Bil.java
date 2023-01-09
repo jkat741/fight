5c49c9fc773fd97aacde476683ba882de3cbf700
Johan Kateby <johanerik.kateby@ntig.se>
2023-01-09 23:15:10 +0100

    Första commiteen

---
+++ src/demo1_prog1/Bil.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo1_prog1;
+
+/**
+ *
+ * @author johanerik.kateby
+ */
+public class Bil {
+    String registreringsnummer;
+    String märke;
+    String modell;
+    int tillverkningsår;
+    
+}
---
+++ src/demo1_prog1/Demo1_prog1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo1_prog1;
+
+
+public class Demo1_prog1 {
+    public static void main(String[] args) {
+
+        
+        Bil min_bil = new Bil();
+        min_bil.registreringsnummer="ABC123";
+        min_bil.märke="Ford";
+        min_bil.modell="Bronco";
+        min_bil.tillverkningsår=1986;
+    }   
+}
+
+class Person{
+    String namn;
+    int ålder;
+}
